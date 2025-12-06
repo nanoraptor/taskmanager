44af5b31162dc284fedc4bd2c5443c321aacb965
nanoraptor <rahul.gyro@gmail.com>
2025-12-06 23:20:36 +0530

    implemented file checking for now

--- tmanager.py
+++ tmanager.py
@@ -2,21 +2,26 @@ import os
 import classes as t
 
 
-print("Welcome to Task Manager")
-print("Please input the location of your *.json file that contains your tasks")
-fname = input().strip().split(" ")
-# if os.path.exists(fname):
-# else;
-#     print("Please enter a valid file path")
-#
-#
-# while True:
-#     break
+print("Welcome to Task Manager!\n")
+print("Please input the location of your *.json file that contains your tasks\n")
+
+fname = ""
+while True:
+    f = input().strip()
+    if not (os.path.exists(f)):
+        print("Please enter a valid file path")
+    else:
+        if f[-5:] != ".json":
+            print("Please enter a valid file path")
+            continue
+        fname = f
+        break
+
+while True:
+    f1 = open(fname, "r+")
+    print(f"\nSuccessfully opened {fname} file and loaded all tasks\n")
+    break
 
 # user input will be given like this ./tmanager.py <options> --optional <string>
 # options can be add, update, list(done | todo | in-progress), mark(-in-progress | -done),
 a = t.Task("a")
-a.add_task("I can live my life!")
-b = a.list_task()
-for i in b:
-    print(f"ID: {i[0]}, Task: {i[1]}")
