f33ef8343079dd9e92909ea3aaa6f1bd3246b160
nanoraptor <rahul.gyro@gmail.com>
2025-12-07 14:03:37 +0530

    More improvements. Updation and Deletion yet to be improved.

--- classes.py
+++ classes.py
@@ -5,44 +5,57 @@ class Task:
         with open(path, "r") as file:
             self.task = file.readlines()
         self.task = list(map(str.strip, self.task))
+        for i in range(len(self.task)):
+            self.task[i] = self.task[i].strip().split(" _ ")
+        for i in range(len(self.task)):
+            self.task[i][0] = int(self.task[i][0])
+        self.idl = []
         for i in self.task:
-            i = i.split(" _ ")
-        for i in self.task:
-            i[0] = int(i[0])
-        temp = []
-        for i in self.task:
-            temp.append(i[0])
-        self.id = max(temp) + 1
+            self.idl.append(i[0])
+        try:
+            self.id = max(self.idl) + 1
+        except ValueError:
+            pass
 
     def add_task(self, tak):
+        ## figure out a way to handle empty tasks
         tak = str(tak)
         self.task.append([self.id, tak])
+        self.idl.append(self.id)
         self.id += 1
         with open(self.path, "w") as file:
-            ttk = self.task
-            for i in ttk:
-                i = str(i[0]) + " _ " + i[1] + "\n"
+            ttk = self.task[:]
+            for i in range(len(ttk)):
+                ttk[i] = str(ttk[i][0]) + " _ " + ttk[i][1] + "\n"
             file.writelines(ttk)
 
     def update_task(self, pid, tak):
         for i in range(len(self.task)):
             if self.task[i][0] == pid:
-                self.task[i][1] = tak
+                self.task[i][1] = str(tak)
         with open(self.path, "w") as file:
-            ttk = self.task
-            for i in ttk:
-                i = str(i[0]) + " _ " + i[1] + "\n"
+            ttk = self.task[:]
+            for i in range(len(ttk)):
+                ttk[i] = str(ttk[i][0]) + " _ " + ttk[i][1] + "\n"
             file.writelines(ttk)
 
     def delete_task(self, pid):
         for i in range(len(self.task)):
             if self.task[i][0] == pid:
                 self.task.pop(i)
+                break
+        self.idl.remove(self.id)
         with open(self.path, "w") as file:
-            ttk = self.task
-            for i in ttk:
-                i = str(i[0]) + " _ " + i[1] + "\n"
+            ttk = self.task[:]
+            for i in range(len(ttk)):
+                ttk[i] = str(ttk[i][0]) + " _ " + ttk[i][1] + "\n"
             file.writelines(ttk)
+
+    def is_id(self, pid):
+        if pid in self.idl:
+            return True
+        else:
+            return False
 
     def list_task(self):
         return self.task
--- tmanager.py
+++ tmanager.py
@@ -8,6 +8,7 @@ print("Please input the location of your *.json file that contains your tasks\n"
 fname = ""
 while True:
     f = input().strip()
+    # f = "test.json"
     if not (os.path.exists(f)):
         print("Please enter a valid file path")
     else:
@@ -18,8 +19,8 @@ while True:
         break
 
 a = t.Task(fname)
-print(f"\nSuccessfully opened {fname} file and loaded all tasks\n")
-print('type "help" for opening help menu')
+print(f"\nSuccessfully opened file: {fname} and loaded all tasks\n")
+print('type "help" for opening help menu\n')
 
 while True:
     uinput = input().lower()
@@ -28,15 +29,19 @@ while True:
         break
     elif uinput == "list":
         ## Implement list
-        pass
+        l = a.list_task()
+        print("All the tasks in the given file:")
+        for i in l:
+            print(f"ID: {i[0]}, Task: {i[1]}\n")
     elif uinput == "add":
         u1 = input()
         a.add_task(u1)
     elif uinput == "update":
+        u1 = ""
         while True:
             u1 = input()
-            if not u1.isnumeric():
-                print("Please give a valid id!")
+            if (not u1.isnumeric()) and (not a.is_id(u1)):
+                print("Please give a valid id!\n")
                 continue
             else:
                 break
@@ -44,10 +49,24 @@ while True:
         u2 = input()
         a.update_task(u1, u2)
     elif uinput == "delete":
-        u2 = int(input())
-        a.delete_task(u2)
+        u1 = ""
+        while True:
+            u1 = input()
+            if (not u1.isnumeric()) and (not a.is_id(u1)):
+                print("Please give a valid id!\n")
+                continue
+            else:
+                break
+        u1 = int(u1)
+        a.delete_task(u1)
     elif uinput == "help":
         ## Implement help menu
-        pass
+        print("Help Menu:\n")
+        print("type 'list to list all tasks\n")
+        print("type 'add' to to add tasks\n")
+        print("type 'update' to add tasks\n")
+        print("type 'delete' to delete tasks\n")
+        print("type 'help' to open this help menu\n")
     else:
-        print("Please give a valid user input!")
+        print("Please give a valid user input!\n")
+        print("Type 'help' for more info\n")
